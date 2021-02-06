a1c64cccfb642ba20db462e47eabfe3cbfdf43d7
SajRashid <sajid.rashid@live.co.uk>
2021-02-06 20:09:33 +0000

    added remarks

--- scripts/fan.py
+++ scripts/fan.py
@@ -2,26 +2,37 @@ from gpiozero import CPUTemperature
 import RPi.GPIO as GPIO
 from time import sleep, strftime, time
 
-pin =2
 cpu = CPUTemperature()
     # Set up input pin
-    
+pin =2   
     # set bcm pin mode 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(pin, GPIO.OUT)
-        # Create an Infinite Loop
-while True:
+        # Create an Infinite Loops
+while True: # Get Initial PIN State
+            # To test the value of a PIN use the .input method
+            # Returns 0 if OFF or 1 if ON
+            pin_state = GPIO.input(pin)  
+            # if the PIN is ON 
+            if pin_state == 1: 
+                 # Set the initial pin state to OFF 
+                GPIO.output(pin, GPIO.LOW)
             print(cpu.temperature)
-            # set pin to off initial
-            GPIO.output(pin, GPIO.LOW)
-            while cpu.temperature > 40:
-                (sleep(1))
-                print(cpu.temperature)
-                 # To test the value of a pin use the .input method
-                pin_is_off = GPIO.input(pin)  # Returns 0 if OFF or 1 if ON
-                if pin_is_off == 0: # true if 1 (ON)
-                   print(cpu.temperature)
-                   GPIO.output(pin, GPIO.HIGH)
-            GPIO.output(pin, GPIO.LOW)
-            (sleep(10))
+           
+            if cpu.temperature > 45:
+
+                while cpu.temperature > 40:
+                    print(cpu.temperature)
+                    # Get the current PIN State, Returns 0 if OFF or 1 if ON
+                    pin_state = GPIO.input(pin) 
+                   
+                    # if PIN state false or 0, the PIN if off then turn the pin ON
+                    if pin_state == 0: 
+                        # Set the PIN state to ON 
+                        GPIO.output(pin, GPIO.HIGH)
+                    (sleep(1))
+                    
+            else:
+                 (sleep(1))
+
